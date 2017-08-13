d0dc96534781d3d206e49b674b4e1199afd729b4
A3018908 <A3018908@BE1XM763.emea1.cds.t-internal.com>
2017-08-13 19:30:45 +0200

    commit me again

--- HelloWorld/src/com/java/examples/HelloWorld.java
+++ HelloWorld/src/com/java/examples/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("This is a branch test");
 		System.out.println("This is the branch test");
+		System.out.println("This is the branch test-2");
 
 	}
 
