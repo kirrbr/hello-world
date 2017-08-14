9126f3096750982734f68c598f9a01ab3302cfce
A3018908 <A3018908@BE1XM763.emea1.cds.t-internal.com>
2017-08-14 10:54:34 +0200

    commit

---
+++ demo/src/main/java/com/example/demo/DemoBean.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBean {
+	
+	public String sendMessage() {
+		return "Message from DemoBean";
+	}
+	
+	
+
+}
